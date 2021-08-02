62144a4381705eee3d23a6d0f52b5acfcc540f2b
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-08-02 22:32:36 +0530

    Create inorder.cpp

---
+++ Tree/inorder.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int key;
+  struct Node *left;
+  struct Node *right;
+   Node(int k)
+  {
+    key=k;
+    left=NULL;
+    right=NULL;
+  }
+};
+void inorder(Node *root)  //left root right
+{
+   if(root!=NULL)
+   {
+     inorder(root->left);
+     cout<<root->key<<" ";
+     inorder(root->right);
+   }
+     
+}
+int main()
+{
+    Node *root=new Node(10);
+    root->left=new Node(20);
+    root->right=new Node(30);
+    root->right->left=new Node(40);
+   	root->right->right=new Node(50);
+    inorder(root);
+    return 0;
+}
+
+
+//output:-20 10 40 30 50 
