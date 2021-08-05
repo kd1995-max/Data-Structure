6207748bb3e813bf617d27c4d412e585f12ff79a
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-08-05 12:31:42 +0530

    Create print_mat.cpp

---
+++ Matrix/print_mat.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int mat[3][2])
+{
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<2;j++)
+		{
+			cout<<mat[i][j]<<" ";
+		}
+	}
+}
+int main() {
+
+ int mat[3][2]={{10,20},
+				{30,40},
+				{50,60}};
+	print(mat);
+	return 0;
+}
+//output:10 20 30 40 50 60 
