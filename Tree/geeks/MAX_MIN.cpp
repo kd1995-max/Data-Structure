b9d765dff7fc4f599fb02ed6914849d07b174b6e
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-08-18 13:09:56 +0530

    Create MAX_MIN.cpp

---
+++ Tree/geeks/MAX_MIN.cpp
@@ -0,0 +1,14 @@
+ int findMax(Node *root)
+    {
+        //code here
+        if(root==NULL) return INT_MIN;
+        else
+        return max(root->data,max(findMax(root->left),findMax(root->right)));
+    }
+    int findMin(Node *root)
+    {
+        //code here
+          if(root==NULL) return INT_MAX;
+        else
+        return min(root->data,min(findMin(root->left),findMin(root->right)));
+    }
