b78d2fa13c55924f7ef04a56d0f910e02e38c947
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-08-31 13:36:49 +0530

    Update floorSqrt.cpp

--- Binary-search-tree/geek/floorSqrt.cpp
+++ Binary-search-tree/geek/floorSqrt.cpp
@@ -4,7 +4,7 @@
          long long low=0, high=x,mid,ans;
     while(low<=high)
     {
-        int mid=(low+high)/2;
+        mid=(low+high)/2;
         if(mid*mid>x)
         {
             high=mid-1;
