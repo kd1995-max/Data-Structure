93f9e75333244266c8976e06cfd5968c2f5671cb
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-09-02 17:36:42 +0530

    Create peekElement.cpp

---
+++ binary_search/peekElement.cpp
@@ -0,0 +1,17 @@
+int peakElement(int arr[], int n)
+{
+   // Your code here
+   int low=0;
+   int high=n-1;
+   while(low<=high)
+   {
+      int mid=(low+high)/2;
+      if((mid==0|| arr[mid-1]<=arr[mid])&&(mid==n-1||arr[mid+1]<=arr[mid]))
+      return mid;
+      if(mid>0 && arr[mid-1]>=arr[mid])
+      high=mid-1;
+      else
+      low=mid+1;
+   }
+   return -1;
+}
