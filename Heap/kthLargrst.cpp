92407467b20e2d7243fc529ff8929df256b4ab99
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-10-25 14:47:12 +0530

    Create kthLargrst.cpp

---
+++ Heap/kthLargrst.cpp
@@ -0,0 +1,19 @@
+ int KthLargest(int arr[], int n, int k) {
+        // Your code here
+         priority_queue<int,vector<int>,greater<int>>p;
+        for(int i=0;i<k;i++)
+            p.push(arr[i]);
+            
+            for(int i=k;i<n;i++)
+            {
+               if(p.top()>arr[i]) 
+               continue;
+               else
+               {
+                   p.pop();
+                   p.push(arr[i]);
+               }
+            }
+            return p.top();
+            
+        }
