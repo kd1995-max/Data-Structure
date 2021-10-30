6498cdcd80c9bf97c6af6641b1e65d14b06a0dbb
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-10-30 12:45:23 +0530

    Create areRotations.cpp

---
+++ String/geek/areRotations.cpp
@@ -0,0 +1,9 @@
+ bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        if(s1.length()!=s2.length())
+        return false;
+        string s3=s1+s1;
+        return ((s3).find(s2)!=string ::npos);
+        
+    }
