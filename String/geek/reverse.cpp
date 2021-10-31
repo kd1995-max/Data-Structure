e072c23755ffac54ae1e6a3b89c8457779a42b46
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-10-31 15:33:13 +0530

    Create reverse.cpp

---
+++ String/geek/reverse.cpp
@@ -0,0 +1,25 @@
+   
+    void reverse(string &str,int low, int high){
+    while(low<=high){
+        swap(str[low],str[high]);
+        low++;
+        high--;
+    }
+}
+    string reverseWords(string S) 
+    { 
+        // code here 
+        int start = 0 , end = 0 ; 
+        for(end = 0; end < S.length(); end++)
+        {
+            if(S[end] == '.')
+            {
+                reverse(S , start , end - 1);
+                start = end + 1;
+            }
+        }
+        
+        reverse(S , start , S.length() - 1);
+        reverse(S , 0 , S.length() - 1);
+        return S;
+    } 
