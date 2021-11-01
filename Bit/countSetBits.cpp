6678fbaaaff75648ebfe54407c7749d053ffd142
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-11-01 12:27:20 +0530

    Create countSetBits.cpp

---
+++ Bit/countSetBits.cpp
@@ -0,0 +1,28 @@
+ int largestPowerOf2InRange(int n)
+    {
+        int x = 0;
+        
+        while((1 << x) <= n)
+        x++;
+        
+        return x - 1;
+    }
+    
+    int countSetBits(int n)
+    {
+        // Your logic here
+        if(n == 0)
+        return 0;
+        
+        int x = largestPowerOf2InRange(n);
+        
+        int btill2x = x * (1 << (x - 1));
+        
+        int msb2xtox = n - (1 << x) + 1;
+        
+        int rest = n - (1 << x);
+        
+        int ans = btill2x + msb2xtox + countSetBits(rest);
+        
+        return ans;
+    }
