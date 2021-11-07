ce3452593f0b177ca372d729e88fb5c1f3dd920f
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-11-07 19:11:34 +0530

    Create reverseString.cpp

---
+++ String/geek/reverseString.cpp
@@ -0,0 +1,12 @@
+  void reverseString(vector<char>& s) {
+        
+        int l=0;
+        int h=s.size()-1;
+        while(l<=h)
+        {
+            swap(s[l],s[h]);
+            l++;
+            h--;
+        }
+        
+    }
