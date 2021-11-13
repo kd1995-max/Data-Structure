73e9c13d1c26c9fb10a29b01dea500fd21fb380f
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-11-13 22:48:54 +0530

    Create findMaxConsecutiveOnes.cpp

---
+++ Array/gfg/findMaxConsecutiveOnes.cpp
@@ -0,0 +1,14 @@
+int findMaxConsecutiveOnes(vector<int>& nums) {
+         int count=0;
+        int max_count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+           if(nums[i]==1)
+               count++;
+            else
+                count=0;
+            max_count=max(max_count,count);
+        }
+        return max_count;
+        
+    }
