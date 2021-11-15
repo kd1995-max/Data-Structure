dc535e68cabe1ee843f8717c6672118d224626e7
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-11-15 12:41:29 +0530

    Create insertAtIndex.cpp

---
+++ Array/gfg/insertAtIndex.cpp
@@ -0,0 +1,10 @@
+void insertAtIndex(int arr[], int sizeOfArray, int index, int element)
+    {
+        //Your code here
+        for(int i=sizeOfArray-1;i>index;i--)
+        {
+            arr[i]=arr[i-1];
+    
+        }
+        arr[index]=element;
+    }
