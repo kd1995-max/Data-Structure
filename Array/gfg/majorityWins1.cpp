bc4fe0cc3bf907393047860fcf56f1c70809ca23
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-11-17 20:23:30 +0530

    Create majorityWins1.cpp

---
+++ Array/gfg/majorityWins1.cpp
@@ -0,0 +1,19 @@
+int majorityWins(int arr[], int n, int x, int y) {
+ 
+    // code here
+
+   int count_x = 0 , count_y = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == x)
+        count_x++;
+        else if(arr[i] == y)
+        count_y++;
+    }
+    int res;
+    if(count_x != count_y)
+    return (count_x > count_y)? x : y;
+    else
+    return (x < y)? x : y;
+    
+}
