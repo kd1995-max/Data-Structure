ea2bdbbca6f6c0fd6d7f74c5f42dcbafa00fc7aa
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-11-26 16:55:02 +0530

    Create effic.cpp

---
+++ Array/gfg/Rearrange Array Alternately/effic.cpp
@@ -0,0 +1,27 @@
+    void rearrange(long long *arr, int n) 
+    { 
+    	
+    	// Your code here
+    	int min_index=0;
+    	int max_index=n-1;
+    	int max=arr[n-1]+1;
+    	
+    	for(int i=0;i<n;i++)
+    	{
+    	    if(i%2==0)
+    	    {
+    	        arr[i]=(arr[max_index]%max)*max+arr[i];
+    	        max_index--;
+    	    }
+    	    else
+    	    {
+    	        arr[i]=(arr[min_index]%max)*max+arr[i];
+    	        min_index++;
+    	    }
+    	}
+    	for(int i=0;i<n;i++)
+    	{
+    	    arr[i] /=max;
+    	}
+    	 
+    }
