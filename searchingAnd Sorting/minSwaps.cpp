932b814375fd5e078ad416e9faffdd0d7fb5459c
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-12-09 13:40:37 +0530

    Create minSwaps.cpp

---
+++ searchingAnd Sorting/minSwaps.cpp
@@ -0,0 +1,25 @@
+	int minSwaps(vector<int>&nums)
+	{
+	    // Code here
+	    int n=nums.size();
+	    vector<pair<int,int>> v(n);
+	    
+	    for(int i=0;i<n;i++)
+	        v[i]={nums[i],i};
+	   
+	     sort(v.begin(),v.end());
+	   
+	     int c=0;
+	     for(int i=0;i<n;i++)
+	     {
+	         if(v[i].second==i)
+	            continue;
+	         else
+	         {
+	             c++;
+	             swap(v[i],v[v[i].second]);
+	             i--;
+	         }
+	     }
+	     return c;   
+	}
