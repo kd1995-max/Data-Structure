2493ffaf1dbaf88b4a803f753522f80114921a42
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-12-09 21:47:54 +0530

    Create inSequence.cpp

---
+++ searchingAnd Sorting/inSequence.cpp
@@ -0,0 +1,13 @@
+ int inSequence(int A, int B, int C){
+        // code here
+        if(C==0)
+        {
+            if(A==B)
+                return 1;
+            else
+                return 0;
+        }
+        
+        int n=1+(B-A)/C;
+        return((B-A)%C==0 && n>0);
+    }
