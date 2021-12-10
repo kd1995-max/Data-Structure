d07b187cb1f150699753b4b03a30ad0b99696cc7
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-12-10 23:05:31 +0530

    Create doUnion.cpp

---
+++ Array/gfg/doUnion.cpp
@@ -0,0 +1,14 @@
+ int doUnion(int a[], int n, int b[], int m)  {
+        //code here  b203
+        set<int> st;
+        for (int i = 0; i < n; i++)
+        {
+            st.insert(a[i]);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            st.insert(b[i]);
+        }
+        return st.size();
+        
+    }
