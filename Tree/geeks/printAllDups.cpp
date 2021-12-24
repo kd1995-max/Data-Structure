7b726da9eb06883f1a12844e9b1d240b225aa7d6
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2021-12-24 16:26:23 +0530

    Create printAllDups.cpp

---
+++ Tree/geeks/printAllDups.cpp
@@ -0,0 +1,31 @@
+string solve(Node * root, unordered_map<string,int> &ma,vector<Node *> &ans)
+{
+    if(root==NULL)
+    return "";
+    string left=solve(root->left,ma,ans);
+    string right=solve(root->right,ma,ans);
+    char parent=root->data;
+    string res=left+parent+right;
+    
+    if(ma.find(res)!=ma.end())
+    {
+        if(ma[res]==1)
+        {
+            ans.push_back(root);
+        }
+    }
+    ma[res]++;
+    return res;
+}
+vector<Node*> printAllDups(Node* root)
+{
+    // Code here
+    vector<Node *> ans;
+    if(root==NULL)
+    return ans;
+    
+    unordered_map<string,int> ma;
+    solve(root,ma,ans);
+    return ans;
+    
+}
