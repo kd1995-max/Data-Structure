31bfe46cd40c365dd70100bdeb66f078be889592
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-01-12 19:53:11 +0530

    Update removeDuplicates.cpp

--- linklist/geek/removeDuplicates.cpp
+++ linklist/geek/removeDuplicates.cpp
@@ -23,3 +23,19 @@ Node *removeDuplicates(Node *head)
     }
     return head;
 }
+
+
+//Function to remove duplicates from sorted linked list.
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+  Node *temp = head;
+    while(temp->next!=NULL){
+        if(temp->data == temp->next->data){
+            temp->next = temp->next->next;
+			continue;
+        }
+        temp=temp->next;
+    }
+    return head;
+}
