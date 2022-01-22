a742bd66066b3a215d85bc262784807358224126
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-01-22 16:19:59 +0530

    Update exchangeColumns.cpp

--- Matrix/geeks/exchangeColumns.cpp
+++ Matrix/geeks/exchangeColumns.cpp
@@ -1,8 +1,8 @@
  void exchangeColumns(vector<vector<int> > &matrix)
     {
         // code here
-         int n1=matrix.size();
-        int m1=matrix[0].size();
+         int n1=matrix.size(); //row
+        int m1=matrix[0].size();//column
         for(int i=0;i<n1;i++)
         swap(matrix[i][0],matrix[i][m1-1]);
     }
