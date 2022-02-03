65a11eb9b102ff737f0f8dde7c9e451a83cd8c74
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-02-03 13:29:56 +0530

    Update mergeHeaps.cpp

--- Heap/gfg/mergeHeaps.cpp
+++ Heap/gfg/mergeHeaps.cpp
@@ -16,3 +16,42 @@
         return vec;
     }
 // log(n+m)*(m+n)
+
+
+
+ void heapify(vector<int> &v,int i,int n)
+    {
+        int largest=i;
+        int l=2*i+1;
+        int r=2*i+2;
+        
+        if(l<n && v[l]> v[largest])
+            largest=l;
+        if(r<n && v[r]> v[largest])
+            largest=r;
+            
+        if(largest !=i)
+        {
+            swap(v[i],v[largest]);
+            heapify(v,largest,n);
+        }
+    }
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        // your code here
+        vector<int> vec;
+        for(int i=0;i<n;i++)
+        vec.push_back(a[i]);
+        
+        for(int i=0;i<m;i++)
+        vec.push_back(b[i]);
+        
+        int x=m+n;
+        
+        for(int i=(x/2)-1;i>=0;i--)
+        heapify(vec,i,x);
+        return vec;
+        
+    }
+
+
+//nlogn
