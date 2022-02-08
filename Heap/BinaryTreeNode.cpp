3b5b15c1e7c20b42ebf8983e90ffd01af8814005
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-02-08 18:49:53 +0530

    Create BinaryTreeNode.cpp

---
+++ Heap/BinaryTreeNode.cpp
@@ -0,0 +1,26 @@
+void toMinHeap(BinaryTreeNode *root,vector<int> &vec,int &idx)
+{
+    if(!root)
+        return;
+    root->data=vec[idx++];
+    toMinHeap(root->left,vec,idx);
+    toMinHeap(root->right,vec,idx);
+    
+}
+void inorder(BinaryTreeNode *root ,vector<int> &vec)
+{
+    if(!root)
+        return;
+    inorder(root->left,vec);
+    vec.push_back(root->data);
+    inorder(root->right,vec);
+}
+BinaryTreeNode* convertBST(BinaryTreeNode* root)
+{
+	// Write your code here.
+    vector<int> vec;
+    inorder(root,vec);
+    int idx=0;
+    toMinHeap(root,vec,idx);
+    return root;
+}
