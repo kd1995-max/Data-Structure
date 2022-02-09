7ac37d07055f0bc2ae184dfe067dc361ba16b2a3
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-02-09 14:20:40 +0530

    Create ispar.cpp

---
+++ String/geek/ispar.cpp
@@ -0,0 +1,39 @@
+ bool ispar(string x)
+    {
+        stack<char> st;
+        for(auto a : x)
+        {
+            if(a=='['||a=='('||a=='{')
+            {
+                st.push(a);
+            }
+            else
+                {
+     
+                        if(st.empty()) 
+                        {
+                           return false;
+                        }
+             
+                        if( st.top() == '[' and a == ']') 
+                        {
+                            st.pop();
+                        }
+                        else if(st.top() == '(' and a == ')') 
+                        {
+                            st.pop();
+                        }
+                        else if(st.top() == '{' and a == '}') 
+                        {
+                            st.pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }                   
+     
+            }
+        }
+        return st.empty() == true;
+
+    }
