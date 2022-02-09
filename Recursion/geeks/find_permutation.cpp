5d30fae1c92cd1a05254c83a465f66aa134832c4
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-02-09 15:43:03 +0530

    Create find_permutation.cpp

---
+++ Recursion/geeks/find_permutation.cpp
@@ -0,0 +1,12 @@
+	vector<string>find_permutation(string S)
+		{
+		    // Code here there
+		    vector<string>vec;
+		    sort(S.begin(),S.end());
+		    vec.push_back(S);
+		    while(next_permutation(S.begin(),S.end()))
+		    {
+		        vec.push_back(S);
+		    }
+		    return vec;
+		}
