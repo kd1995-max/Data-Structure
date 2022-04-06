95a24cd132f7f6fd89de7822baa50b65d7c08bec
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-04-06 19:09:59 +0530

    Update print2largest1.cpp

--- Array/gfg/print2largest1.cpp
+++ Array/gfg/print2largest1.cpp
@@ -1,4 +1,6 @@
- int print2largest(int arr[], int n)
+
+Method:-
+int print2largest(int arr[], int n)
     {
     	int max_1=INT_MIN, max_2=INT_MIN;
     	for(int i=0;i<n;i++)
@@ -22,4 +24,33 @@
     	}
     	return max_2;
     	
+    }
+Method 2:
+ int largest(int arr[],int n)
+   {
+       int max=arr[0];
+       for(int i=1;i<n;i++)
+       {
+           if(arr[i]>max)
+           max=arr[i];
+       }
+       return max;
+   }
+    int print2largest(int arr[], int n)
+    {
+    	//code here.
+    	int larg=largest(arr,n);
+    	int res=-1;
+    	for(int i=0;i<n;i++)
+    	{
+    	    if(arr[i]!=larg)
+    	    {
+    	        if(res==-1)
+    	            res=arr[i];
+    	        else if(arr[i]>res)
+    	        res=arr[i];
+    	    }
+    	   
+    	}
+    	 return res;
     }
