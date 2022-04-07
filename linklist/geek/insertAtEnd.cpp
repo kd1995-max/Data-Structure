23cb3ddd670a990eff4edbd73a17f9f2f1da43ea
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-04-07 18:36:57 +0530

    Update insertAtEnd.cpp

--- linklist/geek/insertAtEnd.cpp
+++ linklist/geek/insertAtEnd.cpp
@@ -1,16 +1,20 @@
- 
+ Node *insertAtBegining(Node *head, int x) {
+        Node *temp = new Node(x);
+        temp->next = head;  
+        return temp;    
+    }
+    
+    
     //Function to insert a node at the end of the linked list.
     Node *insertAtEnd(Node *head, int x)  {
-       // Your code here
-       Node *temp1=new Node(x);
-       if(head==NULL)
-       return temp1;
-       
-       Node *temp=head;
-       while(temp->next!=NULL)
-       {
-           temp=temp->next;
+       Node *newNode = new Node(x);
+       if(head == NULL){
+         return newNode;
        }
-       temp->next=temp1;
+       Node *temp = head;
+       while(temp->next != NULL){
+           temp = temp->next;
+       }
+       temp->next = newNode;
        return head;
     }
