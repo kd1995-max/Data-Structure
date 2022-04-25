003bc8d81a3c72cf0d4aa6c5897a04cb9eeff1e1
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-04-25 11:33:26 +0530

    Update Binary_Tree_to_DLL.cpp

--- Tree/geeks/Binary_Tree_to_DLL.cpp
+++ Tree/geeks/Binary_Tree_to_DLL.cpp
@@ -1,31 +1,27 @@
- void solve(Node *root, Node* &head,Node* &prev,int &f)
-    {
-        if(root==NULL) return;
-        solve(root->left,head,prev,f);
-        if(f==0)
-        {
-            f=1;
-            head=root;
-            prev=root;
-        }
-        else
-        {
-            prev->right=root;
-            prev->right->left=prev;
-            prev=prev->right;
-        }
-        solve(root->right,head,prev,f);
-    }
+ void solve(Node *root, Node* &head,Node * &prev)
+   {
+       if(root==NULL)
+       return;
+          solve(root->left,head,prev);
+       if(head==NULL)
+       {
+           head=root;
+           prev=root;
+       }
+       else
+       {
+           prev->right=root;
+           prev->right->left=prev;
+           prev=prev->right;
+       }
+       solve(root->right,head,prev);
+   }
+   
     Node * bToDLL(Node *root)
     {
         // your code here
-        
         Node *head=NULL;
         Node *prev=NULL;
-        int f=0;
-        solve(root,head,prev,f);
+        solve(root,head,prev);
         return head;
-        
-        
-        
     }
