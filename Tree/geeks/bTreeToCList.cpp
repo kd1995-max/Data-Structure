6e95e6b6d3147eb72c428542b0399b6af9022cbc
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-04-29 11:52:48 +0530

    Update bTreeToCList.cpp

--- Tree/geeks/bTreeToCList.cpp
+++ Tree/geeks/bTreeToCList.cpp
@@ -1,33 +1,32 @@
-   Node* x=NULL;
-   
-   void bTreeToCListutil(Node* root,Node** head)
-   {
-       if(!root)
-           return;
-       
-       bTreeToCListutil(root->left,head);
-       
-       if(!*head)
-           *head=root;
-           
-       root->left=x;
-       
-       if(x)
-           x->right=root;
-           
-       x=root;
-       
-       if(root->right)
-           bTreeToCListutil(root->right,head);
-   }
-   
-   //Function to convert binary tree into circular doubly linked list.
-   Node * bTreeToCList(Node *root)
-   {
-       // your code here
-       Node* head=NULL;
-       bTreeToCListutil(root,&head);
-       x->right=head;
-       head->left=x;
-       return head;
-   }
+    Node* x = NULL;
+    
+    void solve(Node *root,Node **head)
+    {
+        if(!root)
+        return;
+        
+        solve(root->left,head);
+        
+        if(!(*head))
+        *head = root;
+        
+        root->left = x;
+        
+        if(x) 
+            x->right = root;
+            
+        x = root;
+        
+        solve(root->right,head);
+    }
+    Node *bTreeToCList(Node *root)
+    {
+        //add code here.
+        Node *head = NULL;
+        solve(root,&head);
+        
+        x->right = head;
+        head->left = x;
+        
+        return head;
+    }
