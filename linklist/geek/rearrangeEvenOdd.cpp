6c1fbc44598347372d6768c7499f2111c9a18bd3
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-05-26 19:21:21 +0530

    Update rearrangeEvenOdd.cpp

--- linklist/geek/rearrangeEvenOdd.cpp
+++ linklist/geek/rearrangeEvenOdd.cpp
@@ -14,3 +14,35 @@
        odd->next=evenstart;
        
     }
+
+
+//method 2:
+ void rearrangeEvenOdd(Node *head)
+    {
+       // Your Code here
+       Node *even=new Node(-1);
+       Node *even_point=even;
+       Node *odd=new Node(-1);
+       Node *odd_point=odd;
+       
+       int flag=true;
+       while(head)
+       {
+         if(flag)
+         {
+             odd_point->next=head;
+             odd_point=odd_point->next;
+             flag=!flag;
+         }
+         else
+         {
+            even_point->next=head;
+            even_point=even_point->next;
+            flag=!flag;
+         }
+         head=head->next;
+       }
+       
+       odd_point->next=even->next;
+       even_point->next=NULL;
+    }
