88b0194546600da60d1fd7b5246c15f146560023
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-05-26 11:55:20 +0530

    Create pairWiseSwap.cpp

---
+++ linklist/pairWiseSwap.cpp
@@ -0,0 +1,18 @@
+ Node* pairWiseSwap(struct Node* head) 
+    {
+        // The task is to complete this method
+        Node * dummy = new Node(-1);
+        Node * prev = dummy;
+        Node *curr = head;
+        dummy->next = head;
+        
+        while(curr and curr->next)
+        {
+            prev->next=curr->next;
+            curr->next=curr->next->next;
+            prev->next->next=curr;
+            curr=curr->next;
+            prev=prev->next->next;
+        }
+        return dummy->next;
+    }
