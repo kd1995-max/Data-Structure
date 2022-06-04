900f756768516037afb6c30c433473a5d6a3369c
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-06-04 15:24:48 +0530

    Update reverse.cpp

--- String/geek/reverse.cpp
+++ String/geek/reverse.cpp
@@ -1,4 +1,4 @@
-   
+   mathod 1:-
     void reverse(string &str,int low, int high){
     while(low<=high){
         swap(str[low],str[high]);
@@ -23,3 +23,37 @@
         reverse(S , 0 , S.length() - 1);
         return S;
     } 
+method 2:
+string reverseWords(string S) 
+    { 
+        string cpy = S;
+        
+        cpy.push_back('.');
+        
+        stack<string> st;
+        
+        int len = 0;
+        int idx = 0;
+        for(int i = 0; i < cpy.length();i++)
+        {
+            
+            if(cpy[i] == '.')
+            {
+                st.push(cpy.substr(idx , len));
+                len = 0;
+                idx = i + 1;
+            }
+            
+            len++;
+        }
+        
+        string str = "";
+        
+        while(!st.empty())
+        {
+            str += st.top();
+            st.pop();
+        }
+        
+        return str;
+    }
