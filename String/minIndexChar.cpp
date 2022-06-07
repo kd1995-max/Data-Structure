c7caa3661a82fc6f0aa9e95b5fd7368a0d96229e
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-06-07 17:34:54 +0530

    Create minIndexChar.cpp

---
+++ String/minIndexChar.cpp
@@ -0,0 +1,12 @@
+  int minIndexChar(string str, string patt)
+    {
+        // Your code here
+        int n=str.length();
+        for(int i=0;i<n;i++)
+        {
+           if(patt.find(str[i]) !=string::npos)
+           return i;
+        }
+        return -1;
+       
+    }
