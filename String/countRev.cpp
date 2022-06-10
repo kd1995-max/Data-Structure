27686a66a74521670767f3c48fdc5714d1e1d0b4
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-06-10 18:44:17 +0530

    Create countRev.cpp

---
+++ String/countRev.cpp
@@ -0,0 +1,24 @@
+int countRev (string s)
+{
+    // your code here
+    if(s.length() %2 != 0)
+        return -1;
+    int open=0;
+    int close=0;
+    int rev=0;
+    
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]=='{')
+        open++;
+        else
+        {
+            if(open==0)
+                close++;
+            else
+                open--;
+        }
+    }
+    rev=(int)(ceil(open/2.0)+ceil(close/2.0));
+    return rev;
+}
