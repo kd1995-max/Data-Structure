4b9b7ddd2d07ad99f72e07705022f9cef4ba517a
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-06-13 12:58:58 +0530

    Update klargest.cpp

--- Heap/klargest.cpp
+++ Heap/klargest.cpp
@@ -11,3 +11,25 @@
         return v;
         
     }
+------------------
+  vector<int> kLargest(int arr[], int n, int k)
+    {
+        // code here
+        priority_queue<int> pq(arr,arr+n);
+        vector<int> vec;
+        
+        // while(!pq.empty())
+        // {
+        //     if(!k--)
+        //     break;
+            
+        //     vec.push_back(pq.top());
+        //     pq.pop();
+        // }
+        while(k--)
+        {
+             vec.push_back(pq.top());
+             pq.pop();
+        }
+        return vec;
+    }
