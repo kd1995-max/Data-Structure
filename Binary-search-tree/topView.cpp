11f8d1045dff46425b878822aa9adbef9ff33e71
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-06-20 18:25:17 +0530

    Create topView.cpp

---
+++ Binary-search-tree/topView.cpp
@@ -0,0 +1,29 @@
+ vector<int> topView(Node *root)
+    {
+        //Your code here
+        map<int,int> mp;
+        queue<pair<Node *, int>> q;
+        q.push({root,0});
+        while(!q.empty())
+        {
+            Node *curr=q.front().first;
+            int num=q.front().second;
+            
+            if(mp.find(num)== mp.end())
+            mp[num]=curr->data;
+            
+            q.pop();
+            
+            if(curr->left)
+            q.push({curr->left,num-1});
+            
+            if(curr->right)
+            q.push({curr->right,num+1});
+        }
+        vector<int>vec;
+        for(auto e : mp)
+        {
+            vec.push_back(e.second);
+        }
+       return vec; 
+    }
