82752c9a4bdda6ef1ce3b7bc43ff86d33024bc66
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-07-10 18:34:31 +0530

    Create factorial.cpp

---
+++ Array/factorial.cpp
@@ -0,0 +1,30 @@
+  vector<int> factorial(int N)
+{
+        // code here
+        vector<int>res;
+       res.push_back(1);
+       
+       for(int each=2;each<=N;each++)
+       {
+           int carry=0;
+           for(int i=0;i<res.size();i++)
+           {
+               int current=res[i]*each + carry;
+               res[i]=current%10;
+               carry=current/10;
+           }
+           
+           while(carry!=0)
+           {
+               res.push_back(carry%10);
+               carry/=10;
+           }
+           
+       }
+       reverse(res.begin(),res.end());
+       return res;
+
+    }
+Time Complexity: O(nlogn!) n for loop  and logn! for while loop
+Auxiliary Space: O(MAX)
+
