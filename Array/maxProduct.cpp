fff1b68dbb1ff2f755939327cb27f26cef1d254a
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-07-10 17:19:21 +0530

    Update maxProduct.cpp

--- Array/maxProduct.cpp
+++ Array/maxProduct.cpp
@@ -1,4 +1,22 @@
-	long long maxProduct(vector<int> arr, int n) {
+Brute Force :-
+int maxSubArrayProduct(int[] arr, int size) {
+    int maxProduct = INT_MIN
+    for(int i=0 to i < size){
+        localProduct = 1
+        for(int j=i to j < size){
+            localProduct = localProduct * arr[j]
+        }
+        maxProduct = max(maxProduct, localProduct)
+    }
+    return maxProduct
+}
+Time Complexity: O(n²)
+
+Space Complexity: O(1)
+
+optimize:=
+
+long long maxProduct(vector<int> arr, int n) {
 	    // code here
 	    long long ans = arr[0];
 	    long long ma = arr[0];
@@ -17,3 +35,6 @@
 	    
 	    return ans;
 	}
+Time Complexity: O(n)
+
+Space Complexity: O(1)
