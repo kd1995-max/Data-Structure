c5205330c9a62d77f94eaca5ac46e9a91c9f8d84
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-08-07 17:00:23 +0530

    Update bfsOfGraph.cpp

--- graph/bfsOfGraph.cpp
+++ graph/bfsOfGraph.cpp
@@ -1,25 +1,30 @@
- vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+ vector<int> bfsOfGraph(int v, vector<int> adj[]) {
         // Code here
-        bool arr[V+1];
-        memset(arr,false,sizeof(arr));
-        queue<int>q;
-        arr[0]=true;
-        q.push(0);
-        vector<int>vec;
-        while(!q.empty())
-        {
-            int u=q.front();
-            q.pop();
-            
-            vec.push_back(u);
-            for(auto e : adj[u])
-            {
-                if(arr[e]==false)
-                {
-                    arr[e]=true;
-                    q.push(e);
-                }
-            }
-        }
-        return vec;
+        bool visited[v+1];
+       
+        memset(visited,false,sizeof(visited));
+        
+       queue<int> q;
+       visited[0]=true;
+       q.push(0);
+       
+       vector<int>vec;
+       
+       while(!q.empty())
+       {
+           int u=q.front();
+           q.pop();
+           
+           vec.push_back(u);
+           for(auto e : adj[u])
+           {
+               if(visited[e]==false)
+               {
+                   visited[e]=true;
+                   q.push(e);
+               }
+           }
+       }
+       return vec;
+        
     }
