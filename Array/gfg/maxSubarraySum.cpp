43d6141155e5342bc862ff2cae80bcdde60bbd89
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-08-07 10:55:17 +0530

    Update maxSubarraySum.cpp

--- Array/gfg/maxSubarraySum.cpp
+++ Array/gfg/maxSubarraySum.cpp
@@ -1,18 +1,5 @@
-  long long maxSubarraySum(int arr[], int n){
-        
-                // Your code here
-        int maxsum = arr[0];
-        int res = arr[0];
-        for(int i = 1; i< n; i++)
-        {
-            //
-            maxsum = max(maxsum + arr[i] , arr[i]);
-            res = max(res , maxsum);
-        }
-        return res;
-
-    }
- //Function to find the sum of contiguous subarray with maximum sum.
+//1:-   
+//Function to find the sum of contiguous subarray with maximum sum.
     long long maxSubarraySum(int arr[], int n){
         
         // Your code here
@@ -25,4 +12,20 @@
             msum = max(csum,msum);
         }
         return msum;
+    }
+//2:-
+
+long long maxSubarraySum(int arr[], int n){
+        
+                // Your code here
+        int maxsum = arr[0];
+        int res = arr[0];
+        for(int i = 1; i< n; i++)
+        {
+            //
+            maxsum = max(maxsum + arr[i] , arr[i]);
+            res = max(res , maxsum);
+        }
+        return res;
+
     }
