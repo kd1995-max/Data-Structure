b5de5a5f3954ba7ac1219968f537ef6e55bb595e
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-08-07 15:40:10 +0530

    Update majorityElement.cpp

--- Array/gfg/majorityElement.cpp
+++ Array/gfg/majorityElement.cpp
@@ -1,25 +1,28 @@
-  int majorityElement(int a[], int size)
+int majorityElement(int a[], int n)
+{
+      int res=0,c=1;  
+    // your code here
+    for(int i=1;i<n;i++)
     {
-       // your code here
-       int res=0,count=1;
-        for(int i=0;i<size;i++)
+        if(a[i]==a[res])
+        c++;
+        else
+        c--;
+        if(c==0)
         {
-            if(a[res]==a[i])
-            count++;
-            else
-            count--;
-            if(count==0)
-            {
-                res=i;
-                count=1;
-            }
+            res=i;
+            c=1;
         }
-        count=0;
-        for(int i=0;i<size;i++)
-        {
-            if(a[res]==a[i])
-            count++;
-        }
-        if(count<=size/2) return -1;
-        return a[res];
     }
+    c=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==a[res])
+        c++;
+        
+    }
+        if(c<=n/2)
+        return -1;
+        return a[res];
+        
+}
